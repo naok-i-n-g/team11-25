556ee45d553cbad40a5a357216f85c74c8a83350
川阪佐奈恵 <sanae.kawasaka@gmail.com>
2019-11-24 14:33:38 +0900

    限界？

--- assets/js/sanae.js
+++ assets/js/sanae.js
@@ -43,6 +43,16 @@ $("#s10").on("click", function () {
   }
 });
 
+// mouseoverしたらh1のdivがグレーアウト
+// $(".s10").on("mouseover", function(){
+//   $(this).addClass("s22");
+// })
+
+// clickしたら、pが現れる
+// $(".s10").on("click", function(){
+//   // $(this).remove();
+//   $(this).addClass("q1");
+// });
 
 var barChartData = {
     labels: ['2016', '2017', '2018'],
@@ -118,3 +128,59 @@ window.onload = function() {
 //     });
 //     window.myBar.update();
 // });
+
+$("#s8").on("mouseover", function(){
+  $(".s10").fadeIn();
+    $(".s10").css("backgroundColor", "rgb(189, 202, 240)");
+    $(".s22").addClass("q1");
+}); 
+$("#s4").on("mouseover", function(){
+  $(".s10").css("backgroundColor", "rgb(189, 202, 240)");
+  $(".s23").addClass("q1");
+}); 
+$("#s5").on("mouseover", function(){
+  $(".s10").css("backgroundColor", "rgb(189, 202, 240)");
+  $(".s24").addClass("q1");
+}); 
+$("#s6").on("mouseover", function(){
+  $(".s10").css("backgroundColor", "rgb(189, 202, 240)");
+  $(".s25").addClass("q1");
+}); 
+$("#s7").on("mouseover", function(){
+  $(".s10").css("backgroundColor", "rgb(189, 202, 240)");
+  $(".s26").addClass("q1");
+}); 
+
+// 
+$("#i3").on("mouseover", function(){
+  // $("#i2").css("backgroundColor", "rgb(189, 202, 240)");
+  $("#i1").addClass("q1");
+}); 
+
+
+$("#s8").on("click", function(){
+  $(".s22").toggleClass("q2");
+  $(".s22").css("backgroundColor", "rgb(189, 202, 240)");
+});
+$("#s4").on("click", function(){
+  $(".s23").toggleClass("q2");
+  $(".s23").css("backgroundColor", "rgb(189, 202, 240)");
+});
+$("#s5").on("click", function(){
+  $(".s24").toggleClass("q2");
+  $(".s24").css("backgroundColor", "rgb(189, 202, 240)");
+});
+$("#s6").on("click", function(){
+  $(".s25").toggleClass("q2");
+  $(".s25").css("backgroundColor", "rgb(189, 202, 240)");
+});
+$("#s7").on("click", function(){
+  $(".s26").toggleClass("q2");
+  $(".s26").css("backgroundColor", "rgb(189, 202, 240)");
+});
+
+// // 
+// $("#i3").on("click", function(){
+//   $("#i1").toggleClass("q2");
+//   // $("#i1").css("backgroundColor", "rgb(189, 202, 240)");
+// }); 
